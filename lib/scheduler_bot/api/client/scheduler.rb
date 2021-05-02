4babf0183c30d7f7328c85478493a475d30b4519
Egor Iskrenkov <mrblazon@gmail.com>
2021-05-02 17:48:08 +0300

    Command to list available groups

--- lib/scheduler_bot/api/client/scheduler.rb
+++ lib/scheduler_bot/api/client/scheduler.rb
@@ -3,8 +3,13 @@ class SchedulerBot::Api::Client::Scheduler < SchedulerBot::Api::Client::Base
     get('ping')
   end
 
+  def list_groups(telegram_id:)
+    response = get('api/telegram/groups/list', telegram_id: telegram_id)
+    response.successful? ? response.body_hash : []
+  end
+
   def set_group(telegram_id:, group_name:)
-    post('api/telegram/set_group', telegram_id: telegram_id, group_name: group_name).successful?
+    post('api/telegram/groups/set', telegram_id: telegram_id, group_name: group_name).successful?
   end
 
   private
